2246d80136aeec4ee15c3aed94fab08baccb780b
Jack Anderson <jackanderson02@outlook.com>
2025-10-18 15:35:59 +0100

    batman

---
+++ learn-terraform-get-started-aws/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+  cloud {
+    organization = "tf_janderson"
+    workspaces {
+      project = "Learn Terraform"
+      name = "tf_learn"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
