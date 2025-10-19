a4f622a085701fdaf51e36c807a437fbd5d46835
Jack Anderson <jackanderson02@outlook.com>
2025-10-19 22:33:42 +0100

    locals

---
+++ learn-terraform-locals/terraform.tf
@@ -0,0 +1,23 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  cloud {
+    organization = "tf_janderson"
+    workspaces {
+      project = "Learn Terraform"
+      name    = "tf_learn"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+  }
+
+  required_version = ">= 1.1"
+}
+
