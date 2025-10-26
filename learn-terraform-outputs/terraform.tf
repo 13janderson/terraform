9d17a550ee38cbdfa0811bcbf4d4d491a8890742
Jack Anderson <jackanderson02@outlook.com>
2025-10-26 19:44:14 +0000

    outputs

---
+++ learn-terraform-outputs/terraform.tf
@@ -0,0 +1,22 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  cloud {
+    organization = "tf_janderson"
+    workspaces {
+      project = "Learn Terraform"
+      name = "tf_learn"
+    }
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.42.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
